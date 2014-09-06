bc3dfef995cbc17b3a1135b37a4eac5c0839e385
John Lees-Miller <jdleesmiller@gmail.com>
2014-09-06 15:28:34 +0100

    add a script to back up the database

---
+++ private/backup.sh
@@ -0,0 +1,3 @@
+#/usr/bin/env bash
+mongoexport --port 5001 --db meteor --collection entries --csv --fields _id,stamp,text --sort '{stamp:1}' >private/backup/entries_`date -u "+%Y-%m-%dT%H:%M:%SZ"`.csv
+
